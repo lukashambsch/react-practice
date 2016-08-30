2a6cd6aaaf4d6ff29a3364f8db2093084e49595b
Lukas Hambsch <lukas.hambsch@gmail.com>
2016-08-30 12:59:01 -0700

    Added basic IU components.

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
+import Winner from './Winner';
+import Vote from './Vote';
+
+export default React.createClass({
+  mixins: [PureRenderMixin],
+  getPair: function() {
+    return this.props.pair || [];
+  },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function() {
+    return this.props.hasVoted === entry;
+  },
+  render: function() {
+    return <div>
+      {
+        this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />
+      }
+    </div>;
+  }
+})
